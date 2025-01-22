36ec7d41eb5002c413778c17c0aa5650227c0779
sarafernandez11 <sara@nennisiwok.com>
2025-01-23 00:36:46 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
   assetPrefix: isProd ? '/portfolio' : '',
   basePath: isProd ? '/portfolio' : '',
   output: 'export'
